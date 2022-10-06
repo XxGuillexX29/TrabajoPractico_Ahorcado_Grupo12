095dcdd6f8e2473eaf55fc970f9699723f30c7c7
JuanVillagra <villagrajuan99@gmail.com>
2022-10-05 21:54:30 -0300

    Cambios y nueva pagina js creada.Juanvillagra

--- src/components/Jugar.js
+++ src/components/Jugar.js
@@ -1,8 +1,14 @@
 import "../index.css";
 import "../App.css";
+import Ahorcado from "./Ahorcado.js";
+import Hangman from "./Ahorcado.js";
 
-function Jugar()
-{
-
-}
-export default Jugar
+function App() {
+    return (
+      <div className="App">
+        <Hangman />
+      </div>
+    );
+  }
+  
+  export default App;
