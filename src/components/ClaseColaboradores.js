54b2d8312e31e218cdca8fb21f176fd9adbc8082
matias cruz <matiasfc.info@gmail.com>
2022-10-07 00:37:36 -0300

    se agrego icono a la pagina, Cruz Matias

--- src/components/ClaseColaboradores.js
+++ src/components/ClaseColaboradores.js
@@ -3,13 +3,12 @@ class ClaseColaboradores extends React.Component {
     render() {
         return (
             <div >
-
                 <h1>{this.props.Nombre}</h1>
+                <p class="Edad"> {this.props.Edad}</p>
                 <a href="#">
                     <  img class="ImagenColaborador" src={this.props.img} alt="Vista Previa" /></a>
                 <h3 class="Descripcion">Descripcion</h3>
                 <p class="Parrafo"> {this.props.Descripcion}</p>
-
             </div>
         )
     }
