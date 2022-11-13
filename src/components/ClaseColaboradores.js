5a2c35f73e978289893ea57fbd13b89385d34dcb
CruzMatias <matiasfc.info@gmail.com>
2022-11-13 12:27:07 -0300

    Cambios de clases a funciones

--- src/components/ClaseColaboradores.js
+++ src/components/ClaseColaboradores.js
@@ -1,17 +1,16 @@
 import React from "react"
-class ClaseColaboradores extends React.Component {
-    render() {
-        return (
-            <div >
-                <h1>{this.props.Nombre}</h1>
-                <p class="Edad"> {this.props.Edad}</p>
+
+function ClaseColaboradores(props){
+    return(
+        <>
+        <h1>{props.Nombre}</h1>
+                 <p class="Edad"> {props.Edad}</p>
                 <a href="#">
-                <img class="ImagenColaborador" src={this.props.img} alt="Vista Previa" /></a>
+                <img class="ImagenColaborador" src={props.img} alt="Vista Previa" /></a>
                 <h3 class="Descripcion">Descripcion</h3>
-                <p class="Parrafo"> {this.props.Descripcion}</p>
-            </div> 
-        )
-    }
+                <p class="Parrafo"> {props.Descripcion}</p>
+        </>
+    )
 }
 
 
